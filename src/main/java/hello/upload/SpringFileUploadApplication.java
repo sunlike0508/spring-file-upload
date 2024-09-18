73cfe0102d0cfde557ad7ea4fc20ece3ca9fac3f
seonhoshin <sunlike0508@gmail.com>
2024-09-18 14:51:33 +0900

    초기화

---
+++ src/main/java/hello/upload/SpringFileUploadApplication.java
@@ -0,0 +1,13 @@
+package hello.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFileUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFileUploadApplication.class, args);
+    }
+
+}
